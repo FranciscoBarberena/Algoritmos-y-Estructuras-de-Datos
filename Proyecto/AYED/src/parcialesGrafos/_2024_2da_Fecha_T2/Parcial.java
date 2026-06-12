37c3afec972990e53b30877d4d4bff94e8e1a4be
Francisco Barberena <francisco.barberena275991@alumnos.info.unlp.edu.ar>
2026-06-12 20:26:58 -0300

    enunciados faltantes grafos

--- Proyecto/AYED/src/parcialesGrafos/_2024_2da_Fecha_T2/Parcial.java
+++ Proyecto/AYED/src/parcialesGrafos/_2024_2da_Fecha_T2/Parcial.java
@@ -9,6 +9,51 @@ package parcialesGrafos._2024_2da_Fecha_T2;
  *
  * @author Fran
  */
+import java.util.List;
+import tp5.ejercicio01.*;
+import tp1.ejercicio8.Queue;
+
 public class Parcial {
-    
+
+    public Tupla nivelPopularidad(Graph<String> red, String usuario, int distancia, int umbral) {
+        boolean[] marca = new boolean[red.getSize()];
+        Tupla tup = new Tupla(0, false);
+        Vertex<String> vInicial = red.search(usuario);
+        
+        if (vInicial != null) {
+            Queue<Vertex<String>> q = new Queue<>();
+            q.enqueue(vInicial);
+            marca[vInicial.getPosition()] = true;
+            int distanciaActual = 0;
+            
+            while (!q.isEmpty() && distanciaActual <= distancia) {
+                int tamano = q.size();
+                
+                for (int i = 0; i < tamano; i++) {
+                    if (distanciaActual == distancia) {
+                        tup.incrementar();
+                    } else {
+                        Vertex<String> vActual = q.dequeue();
+                        List<Edge<String>> ady = red.getEdges(vActual);
+                        
+                        for (Edge<String> a : ady) {
+                            Vertex<String> vSig = a.getTarget();
+                            int posSig = vSig.getPosition();
+                            
+                            if (!marca[posSig]) {
+                                marca[posSig] = true;
+                                q.enqueue(vSig);
+                            }
+                        }
+                    }
+                }
+                distanciaActual += 1;
+            }
+            tup.setPopular(tup.getAmigos() >= umbral);
+            return tup;
+            
+        } else {
+            return null;
+        }
+    }
 }
--- Proyecto/AYED/src/parcialesGrafos/_2024_2da_Fecha_T2/Tupla.java
+++ Proyecto/AYED/src/parcialesGrafos/_2024_2da_Fecha_T2/Tupla.java
@@ -10,5 +10,31 @@ package parcialesGrafos._2024_2da_Fecha_T2;
  * @author Fran
  */
 public class Tupla {
-    
+    private int amigos;
+    private boolean popular;
+
+    public Tupla(int amigos, boolean popular) {
+        this.amigos = amigos;
+        this.popular = popular;
+    }
+
+    public void incrementar() {
+        this.amigos++;
+    }
+
+    public int getAmigos() {
+        return this.amigos;
+    }
+
+    public void setAmigos(int amigos) {
+        this.amigos = amigos;
+    }
+
+    public boolean isPopular() {
+        return this.popular;
+    }
+
+    public void setPopular(boolean popular) {
+        this.popular = popular;
+    }
 }
